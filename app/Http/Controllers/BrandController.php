62564eef48d66d9048a578b6ea650e8a8063d929
Simvolokov Pavel <psimvolokov@gmail.com>
2026-09-17 13:33:45 +0300

    feat: created brands controller

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,25 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreBrandRequest;
+use App\Http\Resources\BrandResource;
 use App\Models\Brand;
 use Illuminate\Http\Request;
+use OpenApi\Attributes as OA;
 
 class BrandController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    #[OA\Get(
+        path: "/api/brands",
+        summary: "Получить список всех брендов",
+        tags: ["Brands"],
+        responses: [
+            new OA\Response(
+                response: 200,
+                description: "Список брендов получен",
+                content: new OA\JsonContent(
+                    type: "array",
+                    items: new OA\Items(
+                        type: "object",
+                        properties: [
+                            new OA\Property(property: "id", type: "integer", example: 1),
+                            new OA\Property(property: "title", type: "string", example: "Apple")
+                        ]
+                    )
+                )
+            )
+        ]
+    )]
     public function index()
     {
-        //
+        return BrandResource::collection(Brand::all());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    #[OA\Post(
+        path: "/api/brands",
+        summary: "Создать новую категорию",
+        tags: ["Brands"],
+        requestBody: new OA\RequestBody(
+            required: true,
+            content: new OA\JsonContent(
+                required: ["title"],
+                properties: [
+                    new OA\Property(property: "title", type: "string", example: "Apple")
+                ]
+            )
+        ),
+        responses: [
+            new OA\Response(response: 201, description: "Бренд успешно создан"),
+            new OA\Response(response: 422, description: "Ошибка валидации (например, имя уже занято)")
+        ]
+    )]
+    public function store(StoreBrandRequest $request)
     {
-        //
+        $validated = $request->validated();
+        $brand = Brand::create($validated);
+
+        return (new BrandResource($brand))
+            ->response()
+            ->setStatusCode(201);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BrandController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
@@ -7,3 +8,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('products', ProductController::class);
 Route::apiResource('categories', CategoryController::class);
+Route::apiResource('brands', BrandController::class);
